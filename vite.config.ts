f3cf5033b57e84dc4e5aaaf841db327132acbf0e
nlicroshan <nlicroshan@easyops.cn>
2022-08-09 16:42:29 +0800

    eslint config & build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/hooks/index.ts'),
+      name: 'react-hooks',
+      // the proper extensions will be added
+      fileName: 'react-hooks'
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['react'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          react: 'React'
+        }
+      }
+    }
+  }
 })
